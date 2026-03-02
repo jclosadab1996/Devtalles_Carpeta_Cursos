87c68ea1cbae63628e2e58f154465a738711840b
Juan Camilo Losada <jclosadab1996@javerianacali.edu.co>
2026-03-01 19:34:01 -0500

    Add app.js with initial implementation of line number incrementing

---
+++ Programacion_principiantes/progra-101/app.js
@@ -0,0 +1,18 @@
+console.log("Hola Mundo!");
+
+let numberOfLines = 1;
+
+function increaseLineNumber() {
+  numberOfLines += 2;
+}
+
+console.log("Linea #", numberOfLines);
+
+increaseLineNumber();
+console.log("Linea #", numberOfLines);
+
+increaseLineNumber();
+console.log("Linea #", numberOfLines);
+
+increaseLineNumber();
+console.log("Linea #", numberOfLines);
