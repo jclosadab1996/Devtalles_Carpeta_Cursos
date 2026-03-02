7b4ba0dbd9ad82f0736eb382d42ae74f3843bb91
Juan Camilo Losada <jclosadab1996@javerianacali.edu.co>
2026-03-01 21:50:31 -0500

    Add initial TypeScript files and package configuration for progra-101
    - Created package.json with TypeScript and development dependencies.
    - Added basic types demonstration in basic-types.ts.
    - Implemented for loop examples in for.ts.
    - Developed if-else grade evaluation logic in if-else-grades.ts.
    - Created if-else structure examples in if-else.ts.
    - Added loops homework using do-while in loops-homework.ts.
    - Introduced a function to print line numbers in section-2.ts.
    - Implemented switch-case structure for week days in switch.ts.
    - Demonstrated while and do-while loops in while-do-while.ts.

--- Programacion_principiantes/progra-101/app.js
+++ Programacion_principiantes/progra-101/app.js
@@ -4,15 +4,7 @@ let numberOfLines = 1;
 
 function increaseLineNumber() {
   numberOfLines += 2;
+  console.log("Linea #", numberOfLines);
 }
 
-console.log("Linea #", numberOfLines);
-
 increaseLineNumber();
-console.log("Linea #", numberOfLines);
-
-increaseLineNumber();
-console.log("Linea #", numberOfLines);
-
-increaseLineNumber();
-console.log("Linea #", numberOfLines);
